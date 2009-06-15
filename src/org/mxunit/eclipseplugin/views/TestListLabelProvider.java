8671f9350d1fc30b855e20ca062e7c8d8dffed20
marc.esher <marc.esher@f92dd5c0-c831-0410-92f9-1b6876b37263>
2009-06-15 17:58:49 +0000

    git-svn-id: https://mxunit.googlecode.com/svn/org.mxunit.eclipseplugin/trunk@1146 f92dd5c0-c831-0410-92f9-1b6876b37263

--- src/org/mxunit/eclipseplugin/views/TestListLabelProvider.java
+++ src/org/mxunit/eclipseplugin/views/TestListLabelProvider.java
@@ -1,23 +1,24 @@
+
 package org.mxunit.eclipseplugin.views;
 
 import java.text.DecimalFormat;
 
 import org.eclipse.jface.viewers.LabelProvider;
-import org.eclipse.jface.viewers.StyledCellLabelProvider;
-import org.eclipse.jface.viewers.StyledString;
+/*import org.eclipse.jface.viewers.StyledCellLabelProvider;
+import org.eclipse.jface.viewers.StyledString;*/
 import org.eclipse.jface.viewers.ViewerCell;
 import org.eclipse.swt.graphics.Image;
 import org.mxunit.eclipseplugin.model.ITest;
 import org.mxunit.eclipseplugin.model.TestElementType;
 import org.mxunit.eclipseplugin.model.TestStatus;
 
-public class TestListLabelProvider extends StyledCellLabelProvider {
+public class TestListLabelProvider extends LabelProvider {
 	
 	public String getText(Object obj) {
 		return ((ITest)obj).getName();
 	}
 	
-	public void update(ViewerCell cell){
+/*	public void update(ViewerCell cell){
 		ITest el = (ITest) cell.getElement();
 		StyledString styledString = new StyledString(el.toString());
 		styledString.append(" ("+ DecimalFormat.getInstance().format(Math.random()) + " s)",
@@ -26,7 +27,7 @@ public class TestListLabelProvider extends StyledCellLabelProvider {
 		cell.setStyleRanges(styledString.getStyleRanges());
 		cell.setImage(getImage(el));
 		super.update(cell);
-	}
+	}*/
 	
 	public Image getImage(Object obj) {
 		return ResourceManager.getImage( getImageString(obj) );		
